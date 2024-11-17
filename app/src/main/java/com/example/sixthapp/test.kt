44d1055fd81791a5109441bac5dccb81c783f33b
ji-circle <sp202401jw@gmail.com>
2024-11-17 19:01:53 +0900

    first

---
+++ app/src/main/java/com/example/sixthapp/test.kt
@@ -0,0 +1,9 @@
+package com.example.sixthapp
+
+fun main(){
+    println("Job start")
+    println("File 1 download")
+    println("File 1 reading")
+    println("File 2 download")
+    println("File 2 reading")
+}
